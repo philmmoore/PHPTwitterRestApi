c2361d6ce05258d6d5b3d0936ed4898514c772c3
Philip Moore <phil.m@3men.co.uk>
2014-05-20 11:31:59 +0100

    Restructured and made it a bit more DRY

--- twitter-rest-api.php
+++ twitter-rest-api.php
@@ -9,71 +9,46 @@
         public $access_token;
         public $access_token_secret;
         public $oauth;
+        public $api_base_url;
 
-        public function __construct($api_key, $api_secret){
+        public function __construct($api_key, $api_secret, $version = '1.1'){
             $this->api_key = $api_key;
             $this->api_secret = $api_secret;
+            $this->api_base_url = 'https://api.twitter.com/'.$version.'/';
         }
 
         public function setAccessToken($token, $secret){
             $this->access_token = $token;
             $this->access_token_secret = $secret;
-
-            $this->oauth = array(
-                'oauth_consumer_key' => $this->api_key,
-                'oauth_nonce' => time(),
-                'oauth_signature_method' => 'HMAC-SHA1',
-                'oauth_token' => $this->access_token,
-                'oauth_timestamp' => time(),
-                'oauth_version' => '1.0'
-            );
-
         }
 
         public function get($url, $parameters=''){
-            return json_decode($this->curlGET($url, $parameters));
+            return json_decode($this->curlGET($this->buildRequestURL($url), $parameters));
         }
 
         public function post($url, $parameters=''){
-            return json_decode($this->curlPOST($url, $parameters));
+            return json_decode($this->curlPOST($this->buildRequestURL($url), $parameters));
         }
 
-        protected function parseParameters($parameters){
-            $params = '?';
-            $params_array = array();
-            foreach ($parameters as $key => $val){
-                $params_array[] = $key.'='.$val;
-            }
-            $params .= implode('&', $params_array);
-            return $params;
+        public function debug($data){
+            echo "<pre>";
+            print_r($data);
+            echo "</pre>";
         }
 
-        protected function addParamsToOauth($parameters){
-            foreach($parameters as $key => $val){
-                $this->oauth[$key] = $val;
-            }
-            return true;
-        }
+        private function curlPOST($url, $parameters=''){
 
-        protected function curlPOST($url, $parameters=''){
-
-            if (is_array($parameters)){
-                $this->addParamsToOauth($parameters);
-                $parameters = $this->parseParameters($parameters);
-            }
-
-            // Oauth
-            $base_info = $this->buildBaseString($url, 'POST', $this->oauth);
-            $composite_key = rawurlencode($this->api_secret) . '&' . rawurlencode($this->access_token_secret);
-            $oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
-            $this->oauth['oauth_signature'] = $oauth_signature;
-            $header = array($this->buildAuthorizationHeader($this->oauth), 'Expect:');
+            $oauth = $this->setOAuthSignature($url, 'POST', $parameters);
+            $header = array(
+                $this->buildAuthorizationHeader($oauth),
+                'Expect:'
+            );
 
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
             curl_setopt($ch, CURLOPT_HEADER, false);
             curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, ltrim($parameters, '?'));
+            curl_setopt($ch, CURLOPT_POSTFIELDS, (is_array($parameters) ? ltrim($this->parseParameters($parameters), '?') : ''));
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
@@ -84,53 +59,96 @@
 
         }
 
-        protected function curlGET($url, $parameters=''){
+        private function curlGET($url, $parameters=''){
 
-            if (is_array($parameters)){
-                $this->addParamsToOauth($parameters);
-                $parameters = $this->parseParameters($parameters);
-            }
+            $oauth = $this->setOAuthSignature($url, 'GET', $parameters);
+            $header = array(
+                $this->buildAuthorizationHeader($oauth),
+                'Expect:'
+            );
 
-            // Oauth
-            $base_info = $this->buildBaseString($url, 'GET', $this->oauth);
-            $composite_key = rawurlencode($this->api_secret) . '&' . rawurlencode($this->access_token_secret);
-            $oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
-            $this->oauth['oauth_signature'] = $oauth_signature;
-            $header = array($this->buildAuthorizationHeader($this->oauth), 'Expect:');
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($ch, CURLOPT_HEADER, false);
+            curl_setopt($ch, CURLOPT_URL, $url.(is_array($parameters) ? $this->parseParameters($parameters) : ''));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-
-            // Make requests
-            $options = array( CURLOPT_HTTPHEADER => $header,
-                              // CURLOPT_POSTFIELDS => http_build_query($parameters, '', '&'),
-                              CURLOPT_HEADER => false,
-                              CURLOPT_URL => $url.$parameters,
-                              CURLOPT_RETURNTRANSFER => true,
-                              CURLOPT_SSL_VERIFYPEER => false);
-
-            $cn = curl_init();
-            curl_setopt_array($cn, $options);
-            $return = curl_exec($cn);
-            curl_close($cn);
+            $return = curl_exec($ch);
+            curl_close($ch);
 
             return $return;
 
         }
 
-        protected function buildBaseString($baseURI, $method, $params) {
+        private function buildRequestURL($url){
+            return $this->api_base_url.ltrim($url, '/');
+        }
+
+        private function parseParameters($parameters){
+            $params = '?';
+            $params_array = array();
+            foreach ($parameters as $key => $val){
+                $params_array[] = $key.'='.urlencode($val);
+            }
+            $params .= implode('&', $params_array);
+            return $params;
+        }
+
+        private function addParamsToOauth($parameters, $oauth){
+            foreach($parameters as $key => $val){
+                $oauth[$key] = $val;
+            }
+            return $oauth;
+        }
+
+        private function setOAuthSignature($url, $type, $data=''){
+            
+            $oauth = array(
+                'oauth_consumer_key' => $this->api_key,
+                'oauth_nonce' => time(),
+                'oauth_signature_method' => 'HMAC-SHA1',
+                'oauth_token' => $this->access_token,
+                'oauth_timestamp' => time(),
+                'oauth_version' => '1.0'
+            );
+
+            if (is_array($data)){
+                $oauth = $this->addParamsToOauth($data, $oauth);
+            }
+
+            $base_info = $this->buildBaseString($url, $type, $oauth);
+            $composite_key = rawurlencode($this->api_secret) . '&' . rawurlencode($this->access_token_secret);
+            $oauth['oauth_signature'] = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
+
+            return $oauth;
+
+        }
+
+        private function buildBaseString($baseURI, $method, $params) {
+           
             $r = array();
             ksort($params);
+
             foreach($params as $key=>$value){
                 $r[] = "$key=" . rawurlencode($value);
             }
+
             return $method."&" . rawurlencode($baseURI) . '&' . rawurlencode(implode('&', $r));
+
         }
 
-        protected function buildAuthorizationHeader($oauth) {
+        private function buildAuthorizationHeader($oauth) {
+
             $r = 'Authorization: OAuth ';
             $values = array();
-            foreach($oauth as $key=>$value)
+
+            foreach($oauth as $key=>$value){
                 $values[] = "$key=\"" . rawurlencode($value) . "\"";
+            }
+
             $r .= implode(', ', $values);
+
             return $r;
         }
 
